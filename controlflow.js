710dc46b725770a922d68331f22bbe15f77c3042
Husna Afridi <husnashandigul@gmail.com>
2026-08-21 02:36:30 -0700

    add controlflow.js

---
+++ controlflow.js
@@ -0,0 +1,64 @@
+//if
+//if(true)code will execute
+//if(false)code will not execute
+//<(less than),>(greater than),<=(less than or equal to),==(checks value equal),===(checks value and type),
+//!=(not equal to),!==(strict not equal to).
+let a=10;
+let b=5;
+console.log(a<b);//false
+console.log(a>b);//true
+console.log(a<=10);//true
+console.log(a>=11);//false
+console.log(a=="10");//true
+console.log(a==="10");//false
+console.log(a!=b);//true
+console.log(a!=="10");//true
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const temperature=50;
+if(temperature<50){
+    console.log("less than 50");
+} else{
+    console.log("temperature is greater than or equal to 50");   //temperature is greater than or equal to 50
+}
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//if(temperature===41){
+   // console.log("less than 50");
+//} else{
+   // console.log("temperature is greater than 50");
+//}
+//console.log("execute");
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//const score=500
+//if(score>100){
+   // const power="fly"
+   // console.log(`user power:${power}`);
+//}
+//console.log(`user power:${power}`);
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//IMPLICIT SCOPE(SHORT HAND NOTATION)
+//const balance=1000
+//if(balance>50)console.lof("test"),console.log("test 2");//not easy to read
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//NESTING:
+const balance=1000
+if(balance<500){
+    console.log("less than 500");
+} else if(balance<750){
+    console.log("less than 750");
+} else if(balance<900){
+    console.log("less than 900");
+} else{
+    console.log("900 or greater");    //900 or greater
+}
+//=================================================================================================
+const userloggedin=true
+const debitcard=true                      //&& check condition on both side
+if(userloggedin && debitcard){
+    console.log("allow to buy course");   //allow to buy course
+}
+//=============================================;=====================================================
+const loggedinfromgoogle=false;
+const loggedinfromemail=true;
+if(loggedinfromgoogle||loggedinfromemail){
+    console.log("user logged in");  //user logged in
+}
